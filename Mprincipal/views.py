61bee30b0bc8f309a61fe96509b1be458ef5d58e
SimiusLokus <da.novoa@duocuc.cl>
2025-05-19 13:37:15 -0400

    Creacion de servicio API Mprincipal_TEST

--- Mprincipal/views.py
+++ Mprincipal/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .services import obtener_publicaciones, obtener_usuarios, obtener_pedidos
 
-# Create your views here.
+class MenuPrincipalView(APIView):
+    def get(self, request):
+        return Response({
+            "secciones": [
+                {"nombre": "Publicaciones", "endpoint": "/menu/publicaciones/"},
+                {"nombre": "Usuarios", "endpoint": "/menu/usuarios/"},
+                {"nombre": "Pedidos", "endpoint": "/menu/pedidos/"},
+            ]
+        })
+
+class PublicacionesView(APIView):
+    def get(self, request):
+        data = obtener_publicaciones()
+        return Response(data)
+
+class UsuariosView(APIView):
+    def get(self, request):
+        data = obtener_usuarios()
+        return Response(data)
+
+class PedidosView(APIView):
+    def get(self, request):
+        data = obtener_pedidos()
+        return Response(data)
